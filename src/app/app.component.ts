c861684acac534548df022e4961373ee8cdee2ea
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-17 18:05:51 +0530

    reading object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,19 @@ import { Observable, Subscription } from 'rxjs';
 export class AppComponent {
 
   courses$: Observable<any[]>;
-
+  course$: Observable<any>;
+  author$: Observable<any>;
 
   constructor(db: AngularFireDatabase) {
     this.courses$ = db.list('/courses').valueChanges();
+    this.course$ = db.object('/courses/1').valueChanges();
+    console.log(this.course$);
+    this.author$ = db.object('/authors/1').valueChanges();
+
+
   }
+
+
 
 
 }
